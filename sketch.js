f027ea97e7b020b58f0a1df897c5dc3c62caa9a4
Han <59415595+Han001020@users.noreply.github.com>
2023-07-30 03:40:44 +0900

    p5js setup

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+class Point
+{
+  
+}
+
+function draw() {
+  resizeCanvas(windowWidth, windowHeight)
+  background(220);
+  circle(width/2,height/2,100)
+  
+}
