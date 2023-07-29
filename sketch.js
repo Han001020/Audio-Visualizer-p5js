4749dbf9af82043a70b0912abc363a5c70a97d62
Han <59415595+Han001020@users.noreply.github.com>
2023-07-30 04:50:19 +0900

    create wave

--- sketch.js
+++ sketch.js
@@ -1,15 +1,46 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+var song
+var fft
+
+function preload()
+{
+  song = loadSound('music/The Lake Of Oblivion.mp3')
+  song.setLoop(true)
 }
 
-class Point
-{
-  
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  fft =new p5.FFT()
 }
 
 function draw() {
-  resizeCanvas(windowWidth, windowHeight)
-  background(220);
-  circle(width/2,height/2,100)
-  
+  background(0)
+  stroke(255)
+  noFill()
+  var wave = fft.waveform()
+
+  beginShape()
+  for(var i = 0; i < width; i++)
+  {
+    var index = floor(map(i,0,width,0,wave.length))
+
+    var x = i
+    var y = wave[index] * 200 + height / 2
+    vertex(x,y)
+  }
+
+  endShape()
+}
+
+function mouseClicked()
+{
+  if(song.isPlaying())
+  {
+    song.pause()
+    noLoop()
+  }
+  else
+  {
+    song.play()
+    loop()
+  }
 }
